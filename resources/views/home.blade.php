1bbb993b790bf268b60dc84d877f45ec561c28cd
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-11 17:17:51 +0200

    stampato a schermno tramite view i film

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+   <h1>FILM DI SCARPA:</h1>
+    <ul>
+        @foreach ($movies as $movie)
+        <li>Titolo:{{$movie['title']}}</li> 
+        <li>Titolo originale:{{$movie['original_title']}}</li> 
+        <li>Lingua:{{$movie['original_title']}}</li>
+        <hr> 
+        @endforeach
+    </ul>
+    
+</body>
+</html>
