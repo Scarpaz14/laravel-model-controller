749ed9252b27b0f6aa604661030899c3f0906bcb
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-11 16:48:25 +0200

    creati componenti model,controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie;
+
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        $movies = Movie::all();
+        dd($movies);
+    }
 }
